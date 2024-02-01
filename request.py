f543489caf83eb621d4a7ee6c0a22f63d539ae6d
AlexKlaus <krounix@gmail.com>
2024-02-01 08:14:07 +0300

    add comments

--- request.py
+++ request.py
@@ -7,9 +7,13 @@ import config
 
 def get_city_coord(city):
     """Получение координат города"""
+    # Задаем параметры запроса для получения координат
     payload = {"geocode": city, "apikey": config.geo_key, "format": "json"}
+    # Отправляем GET-запрос к API Yandex Maps Geocoding
     r = requests.get("https://geocode-maps.yandex.ru/1.x", params=payload)
+    # Преобразуем ответ в JSON-формат
     geo = json.loads(r.text)
+    # Извлекаем координаты из полученного JSON
     return geo["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
         "Point"
     ]["pos"]
